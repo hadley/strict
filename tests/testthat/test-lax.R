f571dd699b626fd01f43adf93cd8577b8c18e7c1
Karl Dunkle Werner <karldw@users.noreply.github.com>
2025-05-26 09:35:35 -0400

    Replace env_bind_fns with env_bind_active (#39)
    * Replace env_bind_fns with env_bind_active
    
    * Fix unrelated test regex

--- tests/testthat/test-lax.R
+++ tests/testthat/test-lax.R
@@ -2,6 +2,6 @@ context("lax")
 
 test_that("lax suppresses warnings too", {
   df <- data.frame(xyz = 1)
-  expect_warning(df$x, "Partial match")
+  expect_warning(df$x, "partial match")
   expect_warning(lax(df$x), NA)
 })
